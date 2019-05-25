acf2e9716627f4c1544b75ec804211fd76c3674e
bluebean <1401597377@qq.com>
2019-05-25 23:32:19 +0800

    new

--- Box2DLiteUnity3D/Assets/Box2DLite/Collide.cs
+++ Box2DLiteUnity3D/Assets/Box2DLite/Collide.cs
@@ -4,10 +4,40 @@ using Vec2 = UnityEngine.Vector2;
 
 namespace bluebean.Box2DLite
 {
+  
+    // Box vertex and edge numbering:
+    //
+    //        ^ y
+    //        |
+    //        e1
+    //   v2 ------ v1
+    //    |        |
+    // e2 |        | e4  --> x
+    //    |        |
+    //   v3 ------ v4
+    //        e3
+
+    public enum Axis
+    {
+        FACE_A_X,
+        FACE_A_Y,
+        FACE_B_X,
+        FACE_B_Y,
+    }
+
+    public enum EdgeNumbers
+    {
+        NO_EDGE = 0,
+        EDGE1,
+        EDGE2,
+        EDGE3,
+        EDGE4,
+    }
+
     public struct ClipVertex
     {
-        Vec2 v;
-        FeaturePair feature;
+        public Vec2 v;
+        public FeaturePair feature;
     }
 
     public static class Collide
@@ -17,6 +47,72 @@ namespace bluebean.Box2DLite
             MathUtils.Swap<char>(ref fp.inEdge1, ref fp.inEdge2);
             MathUtils.Swap<char>(ref fp.outEdge1, ref fp.outEdge2);
         }
-        
+
+        /// <summary>
+        /// 返回incident box的碰撞边的顶点
+        /// </summary>
+        /// <param name="clipVertices">返回的incident box的碰撞边的顶点</param>
+        /// <param name="size">incident box的尺寸</param>
+        /// <param name="pos">incident box的位置</param>
+        /// <param name="rot">incident box的旋转</param>
+        /// <param name="normal">指向incident box的碰撞法线</param>
+        static void ComputeIncidentEdge(out ClipVertex[] clipVertices, Vec2 size, Vec2 pos, Mat22 rot, Vec2 normal)
+        {
+            clipVertices = new ClipVertex[2];
+            //将碰撞法线转换到incident box的本地坐标系
+            Mat22 RotT = rot.Transpose();
+            Vec2 n = -(rot * normal);
+            Vec2 nAbs = n.Abs();
+            //todo 貌似有bug,如果不是正方形
+            if(nAbs.x > nAbs.y)
+            {
+                if (MathUtils.Sign(n.x) > 0)
+                {
+                    //边4
+                    clipVertices[0].v.Set(size.x, -size.y);
+                    clipVertices[0].feature.inEdge2 = (char)EdgeNumbers.EDGE3;
+                    clipVertices[0].feature.outEdge2 = (char)EdgeNumbers.EDGE4;
+                    clipVertices[1].v.Set(size.x, size.y);
+                    clipVertices[1].feature.inEdge2 = (char)EdgeNumbers.EDGE4;
+                    clipVertices[1].feature.outEdge2 = (char)EdgeNumbers.EDGE1;
+                }
+                else
+                {
+                    //边2
+                    clipVertices[0].v.Set(-size.x, size.y);
+                    clipVertices[0].feature.inEdge2 = (char)EdgeNumbers.EDGE1;
+                    clipVertices[0].feature.outEdge2 = (char)EdgeNumbers.EDGE2;
+                    clipVertices[1].v.Set(-size.x, -size.y);
+                    clipVertices[1].feature.inEdge2 = (char)EdgeNumbers.EDGE2;
+                    clipVertices[1].feature.outEdge2 = (char)EdgeNumbers.EDGE3;
+                }
+            }
+            else
+            {
+                if (MathUtils.Sign(n.y) > 0)
+                {
+                    //边1
+                    clipVertices[0].v.Set(size.x, size.y);
+                    clipVertices[0].feature.inEdge2 = (char)EdgeNumbers.EDGE4;
+                    clipVertices[0].feature.outEdge2 = (char)EdgeNumbers.EDGE1;
+                    clipVertices[1].v.Set(-size.x, size.y);
+                    clipVertices[1].feature.inEdge2 = (char)EdgeNumbers.EDGE1;
+                    clipVertices[1].feature.outEdge2 = (char)EdgeNumbers.EDGE2;
+                }
+                else
+                {
+                    //边3
+                    clipVertices[0].v.Set(-size.x, -size.y);
+                    clipVertices[0].feature.inEdge2 = (char)EdgeNumbers.EDGE2;
+                    clipVertices[0].feature.outEdge2 = (char)EdgeNumbers.EDGE3;
+                    clipVertices[1].v.Set(size.x, -size.y);
+                    clipVertices[1].feature.inEdge2 = (char)EdgeNumbers.EDGE3;
+                    clipVertices[1].feature.outEdge2 = (char)EdgeNumbers.EDGE4;
+                }
+            }
+            //转换到世界坐标系
+            clipVertices[0].v = pos + rot * clipVertices[0].v;
+            clipVertices[1].v = pos + rot * clipVertices[1].v;
+        }
     }
 }
--- Box2DLiteUnity3D/Assets/Box2DLite/MathUtils.cs
+++ Box2DLiteUnity3D/Assets/Box2DLite/MathUtils.cs
@@ -1,9 +1,96 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace bluebean.Box2DLite
 {
+    public struct Vec2
+    {
+        public float x, y;
+
+        public Vec2(float _x, float _y) {
+            x = _x;
+            y = _y;
+        }
+
+        public void Set(float _x, float _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public float Length()
+        {
+            return (float)Math.Sqrt(x * x + y * y);
+        }
+
+        public Vec2 Abs()
+        {
+            return new Vec2(Math.Abs(x), Math.Abs(y));
+        }
+
+        public static Vec2 operator -(Vec2 v)
+        {
+            v.x = -v.x;
+            v.y = -v.y;
+            return v;
+        }
+
+        public static Vec2 operator +(Vec2 v1, Vec2 v2)
+        {
+            Vec2 v = new Vec2(v1.x + v2.x, v1.y + v2.y);
+            return v;
+        }
+
+        public static Vec2 operator -(Vec2 v1, Vec2 v2)
+        {
+            Vec2 v = new Vec2(v1.x - v2.x, v1.y - v2.y);
+            return v;
+        }
+    }
+
+    public struct Mat22
+    {
+        public Vec2 col1;
+        public Vec2 col2;
+
+        public Mat22(float angle)
+        {
+            float c = (float)Math.Sin(angle);
+            float s = (float)Math.Cos(angle);
+            col1.x = c;col2.x = -s;
+            col1.y = s;col2.y = c;
+        }
+
+        public Mat22(Vec2 _col1, Vec2 _col2)
+        {
+            col1 = _col1;
+            col2 = _col2;
+        }
+
+        public Mat22 Transpose()
+        {
+            return new Mat22(new Vec2(col1.x, col2.x), new Vec2(col1.y, col2.y));
+        }
+
+        public Mat22 Invert()
+        {
+            float a = col1.x;
+            float b = col2.x;
+            float c = col1.y;
+            float d = col2.y;
+            float det = a*d -b * c;
+            det = 1.0f / det;
+            Mat22 B = new Mat22(new Vec2(det * d, -det * c), new Vec2(-det * b, det * a));
+            return B;
+        }
+
+        public static Vec2 operator *(Mat22 mat22, Vec2 v)
+        {
+            return new Vec2(mat22.col1.x * v.x + mat22.col2.x * v.y, mat22.col1.y * v.x + mat22.col2.y * v.y);
+        }
+    }
+
     public static class MathUtils
     {
         public static void Swap<T>(ref T a, ref T b)
@@ -11,6 +98,11 @@ namespace bluebean.Box2DLite
             T tmp = a;
             a = b;
             b = tmp;
+        }
+
+        public static float Sign(float x)
+        {
+            return x < 0 ? -1f : 1f;
         }
     }
 }
